2e8e99785c3235161ee187178b667fec543322c2
saalim <saalimmixon@gmail.com>
2021-01-15 06:16:04 -0500

    initial commit of files

---
+++ Saalim/index.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".#toggle");
+const toggleLinks = document.querySelector(".toggle-links");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
